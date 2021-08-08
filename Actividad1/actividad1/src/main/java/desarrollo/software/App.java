a9dd96554df7f3efc663700aeace6269b6abf6f7
GGranadosEAN <ggranad55083@universidadean.edu.co>
2021-08-08 15:10:18 -0500

    Ejercicios Guia 1

---
+++ Actividad1/actividad1/src/main/java/desarrollo/software/App.java
@@ -0,0 +1,13 @@
+package desarrollo.software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Guia 1 - Actividad 1" );
+    }
+}
---
+++ Actividad1/actividad1/src/main/java/desarrollo/software/punto6/Estudiante.java
@@ -0,0 +1,9 @@
+package desarrollo.software.punto6;
+
+import java.util.Date;
+
+public class Estudiante extends Persona{
+    public int semestreActual;
+    public Carrera carreraActual;
+    public Date fechaIngreso;
+}
